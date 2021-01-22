3ee2c0479bccd33f8686a247a9cfe6289798b3b7
TomCorson <corsontom@yahoo.com>
2021-01-21 22:39:46 -0500

    update api url

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,3 +1,3 @@
-//export const API_URL = 'https://zcw-blog-project-backend.herokuapp.com';
+export const API_URL = 'https://zcw-blog-project-backend.herokuapp.com';
 
-export const API_URL = 'http://localhost:8080';
+ // export const API_URL = 'http://localhost:8080';
